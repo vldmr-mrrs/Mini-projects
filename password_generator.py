56f2c94567e4b390320838cd30fb48ee236bcf1c
vldmr-mrrs <89467807+vldmr-mrrs@users.noreply.github.com>
2022-05-06 19:22:35 +0300

    Add password_generator

---
+++ password_generator.py
@@ -0,0 +1,40 @@
+import random
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+
+chars = ''
+
+num_of_passwords = int(input('Введите количество паролей для генерации: '))
+length_password = int(input('Введите длину пароля: '))
+add_digit = input('Включить цифры? (д = да, н = нет) ').strip()
+add_lowercase = input('Включить прописные буквы? (д = да, н = нет) ').strip()
+add_uppercase = input('Включить строчные буквы? (д = да, н = нет) ').strip()
+add_punctuation = input('Включить символы, такие как !#$%&*+-=?@^_? (д = да, н = нет) ').strip()
+remove_badsymbols = input('Исключить символы il1Lo0O? (д = да, н = нет)').strip()
+
+if add_digit.lower() == 'д':
+    chars += digits
+if add_lowercase.lower() == 'д':
+    chars += lowercase_letters
+if add_uppercase.lower() == 'д':
+    chars += uppercase_letters
+if add_punctuation.lower() == 'д':
+    chars += punctuation
+if remove_badsymbols.lower() == 'д':
+    for c in 'il1Lo0O':
+        chars = chars.replace(c, '')
+
+
+def generate_password(length, chars):
+    password = ''
+    for j in range(length):
+        password += random.choice(chars)
+    print('Вот ваш пароль', password)
+    return password
+
+
+for _ in range(num_of_passwords):
+    generate_password(length_password, chars)
